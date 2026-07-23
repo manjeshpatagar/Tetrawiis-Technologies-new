0361d9f9684de20c6392f4767b7ff13efcf2dab1
ganies-code <hosamani.ganesh@gmail.com>
2026-07-23 11:59:35 +0530

    aded the home page navigation

--- ui/src/app/robots.ts
+++ ui/src/app/robots.ts
@@ -1,8 +1,14 @@
 import type { MetadataRoute } from 'next';
 import { siteConfig } from '@/constants/site';
+
+export const dynamic = 'force-static';
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/' },
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
     sitemap: `${siteConfig.url}/sitemap.xml`,
   };
 }
